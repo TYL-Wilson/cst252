eae37d510e7bcb084f54f8ceb4fbf96dbe25a0ca
Tyler <tylwilson@csumb.edu>
2020-09-14 13:43:51 -0700

    Update lab.js

---
+++ lab4/js/lab.js
@@ -0,0 +1,7 @@
+//psuedocode for web based 2D game <br/>
+  //design art assets, music, & sounds. <br/>
+  //program js game mechanics such as movement & combat <br/>
+  //implement assets in tandem with game mechanics <br/>
+  //program calculations for end of game score <br/>
+  //create restart condition- after failing, the game restarts <br/>
+  //polish out bugs and errors; create a stable flow. <br/>
