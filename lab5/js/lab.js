851703d77802dd51825823c10f449ab6981fd8bd
Tyler <tylwilson@csumb.edu>
2020-09-14 17:30:35 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,16 @@
+// Define Variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
+
+
+// Calculate
+age = 2019 - year;
+
+// Output
+var make = document.writeln("Make: " + make + "<br>");
+var model = document.writeln("Model: " + model + "<br>");
+var color = document.writeln("Color: " + color + "<br>");
+var year = document.writeln("Year: " + year + "<br>");
+var age = document.writeln("Age: " + age + " year<br>");
