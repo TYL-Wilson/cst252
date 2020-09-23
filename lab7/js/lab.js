218e13370d58939b1709695518678100dcfbd659
Tyler <tylwilson@csumb.edu>
2020-09-22 19:50:18 -0700

    lab 7 update

---
+++ lab7/js/lab.js
@@ -0,0 +1,28 @@
+//Lab 7: Functions
+
+//enter username
+var username = prompt("What's your username?");
+console.log("Your username is:"), console.log(username);
+
+//define copy array & split/sort
+var usernameSplit = username.split("");
+usernameSplit.split;
+usernameSplit.sort();
+
+//linebreak
+console.log();
+
+//print sorted username
+console.log("Your sorted username is:");
+console.log(usernameSplit);
+
+document.writeln("Your sorted username is:");
+document.writeln(usernameSplit);
+
+
+//resources:
+// W3Schools: https://www.w3schools.com/jsref/met_win_prompt.asp , https://www.w3schools.com/js/js_arrays.asp
+// developer.mozilla: https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+// stackoverflow: https://stackoverflow.com/questions/40824536/stop-extra-comma-and-space-from-being-printed-out-at-end-of-each-array
+// includehelp: https://www.includehelp.com/code-snippets/create-a-user-defined-array-in-javascript.aspx
+// tutorialspoint: https://www.tutorialspoint.com/in-javascript-how-to-empty-an-array
