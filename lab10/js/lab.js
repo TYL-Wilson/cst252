ce42091b5e3bd2453680f28a645de6b0aa208955
Tyler <tylwilson@csumb.edu>
2020-10-05 22:09:25 -0700

    lab10

---
+++ lab10/js/lab.js
@@ -0,0 +1,19 @@
+//Lab 10 JS
+var input = prompt("")
+
+const button = document.querySelector('input');
+const paragraph = document.querySelector('p');
+
+button.addEventListener('click', updateButton);
+
+function updateButton() {
+  if (button.value === 'Hello') {
+    button.value = 'Goodbye, ';
+    paragraph.textContent = input;
+  } else {
+    button.value = 'Hello';
+    paragraph.textContent = 'Goodbye, ';
+  }
+}
+
+// source: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/button
