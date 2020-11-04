c983c6c2e4643b922f26465a4147e4e6a7336d5f
Tyler <tylwilson@csumb.edu>
2020-11-04 15:47:08 -0800

    18

---
+++ lab17/js/lab.js
@@ -0,0 +1,17 @@
+//lab 16: libraries / JQuery
+
+// $(".elementid").command   <-- replaces getElementById /w JQuery
+
+// $(".output").html("This is some text.");
+
+// sideElement = $("#sideoutput");
+
+// $("#output").append("<button>Click Me");
+
+
+$("#output").append("<button id='button'>button");
+
+$("#button").click(function() {
+  prompt("Make button green?");
+  $("button").css({"background-color": "green"});
+})
